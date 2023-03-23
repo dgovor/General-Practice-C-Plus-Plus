be160067923ef7a191374a5dd939095589108194
Dmitrii Govor <85813394+dgovor@users.noreply.github.com>
2023-03-23 04:54:24 -0700

    Test repo

--- test.cpp
+++ test.cpp
@@ -4,6 +4,7 @@
 int main()
 {
     std::string str;
+    std::cout << "This is a test repo. Please find my full projects in pins." << std::endl;
     std::cout << "Please enter your name:" << std::endl;
     std::cin >> str;
     std::cout << "Hello " << str << std::endl;
