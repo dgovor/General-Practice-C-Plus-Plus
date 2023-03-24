150ede916f672bdf926727ea98fc358a42b99cea
Dmitrii Govor <dmitrii.m.govor@gmail.com>
2023-03-24 04:04:37 -0700

    Test grounds

--- test.cpp
+++ test.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
-#include <string>
+
+int age[4];
+float temper[] = {31.5, 32.7, 38.9};
 
 int main()
 {
-    std::string str;
-    std::cout << "This is a test repo. Please find my full projects in pins." << std::endl;
-    std::cout << "Please enter your name:" << std::endl;
-    std::cin >> str;
-    std::cout << "Hello " << str << std::endl;
-    return(0);
+    age[0] = 25;
+    age[1] = 20;
+    age[2] = 19;
+    age[3] = 19;
+    float *new_one;
+    new_one = temper;
+
+    std::cout << "Age = " << new_one[0] << std::endl;
+    return (0);
 }
