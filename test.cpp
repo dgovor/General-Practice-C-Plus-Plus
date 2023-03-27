b16f576976889387515158276153e838d90c015f
Dmitrii Govor <dmitrii.m.govor@gmail.com>
2023-03-27 05:45:33 -0700

    float, int, uint

--- test.cpp
+++ test.cpp
@@ -1,22 +1,36 @@
 #include <iostream>
+#include <cstdint>
 
 #define DEBUG
-
-int age[4];
-float temper[] = {31.5, 32.7, 38.9};
+float flt = -7.44f;
+int32_t sgn;
+uint32_t unsgn;
 
 int main()
 {
     std::cout << "This is a testing ground repo. Please find my full projects in pinned repos." << std::endl;
 
-    age[0] = 25;
-    age[1] = 20;
-    age[2] = 19;
-    age[3] = 19;
-    float *new_one;
-    new_one = temper;
+    sgn = flt;
+    unsgn = flt;
+
+    int fahrenheit = 100;
+    int celsius;
+    celsius = (5.0 / 9) * (fahrenheit - 32);
+
+    float weight = 10.99;
+
 #ifdef DEBUG
-    std::cout << "Age = " << new_one[0] << std::endl;
+    std::cout << "Float:" << flt << std::endl;
+    std::cout << "Integer:" << sgn << std::endl;
+    std::cout << "Unsigned integer:" << unsgn << std::endl;
+
+    std::cout << "Fahrenheit:" << fahrenheit << std::endl;
+    std::cout << "Celsius:" << celsius << std::endl;
+
+    std::cout << "Float:" << weight << std::endl;
+    std::cout << "Integer part:" << (int)weight << std::endl;
+    std::cout << "Fractional part:" << (int)((weight - (int)weight) * 10000) << std::endl;
 #endif
+
     return (0);
 }
