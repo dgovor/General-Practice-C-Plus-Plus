ad07e5a97f44fc70ce9c036d7a20774f071026af
Dmitrii Govor <dmitrii.m.govor@gmail.com>
2023-03-28 03:00:43 -0700

    Average

--- test.cpp
+++ test.cpp
@@ -2,34 +2,24 @@
 #include <cstdint>
 
 #define DEBUG
-float flt = -7.44f;
-int32_t sgn;
-uint32_t unsgn;
+#define ARRAY_SIZE 5
 
 int main()
 {
     std::cout << "This is a testing ground repo. Please find my full projects in pinned repos." << std::endl;
 
-    sgn = flt;
-    unsgn = flt;
+    float average;
 
-    int fahrenheit = 100;
-    int celsius;
-    celsius = (5.0 / 9) * (fahrenheit - 32);
+    int new_array[ARRAY_SIZE] = {3, 7, 12, 15, 34};
 
-    float weight = 10.99;
+    for (int i = 0; i < ARRAY_SIZE; i++)
+    {
+        average += new_array[i];
+    }
+    average /= ARRAY_SIZE;
 
 #ifdef DEBUG
-    std::cout << "Float:" << flt << std::endl;
-    std::cout << "Integer:" << sgn << std::endl;
-    std::cout << "Unsigned integer:" << unsgn << std::endl;
-
-    std::cout << "Fahrenheit:" << fahrenheit << std::endl;
-    std::cout << "Celsius:" << celsius << std::endl;
-
-    std::cout << "Float:" << weight << std::endl;
-    std::cout << "Integer part:" << (int)weight << std::endl;
-    std::cout << "Fractional part:" << (int)((weight - (int)weight) * 10000) << std::endl;
+    std::cout << "Average of the array = " << average << std::endl;
 #endif
 
     return (0);
